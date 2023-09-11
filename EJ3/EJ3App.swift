a0d6e7d3dc14be88a0408fbe094472481d36120a
andreassalazar <andrea.sosa01@iest.edu.mx>
2023-09-11 17:47:22 -0600

    Initial Commit

---
+++ EJ3/EJ3App.swift
@@ -0,0 +1,17 @@
+//
+//  EJ3App.swift
+//  EJ3
+//
+//  Created by -a.s  on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct EJ3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
